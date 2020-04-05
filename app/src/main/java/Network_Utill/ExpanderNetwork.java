12be86e73529cc6b1bc8775bb54697902c86deb4
LeeJuWan <49786050+LeeJuWan@users.noreply.github.com>
2020-04-05 17:29:46 +0900

    2020.04.05 NetworkSecurityConfig repair

--- app/src/main/java/Network_Utill/ExpanderNetwork.java
+++ app/src/main/java/Network_Utill/ExpanderNetwork.java
@@ -2,10 +2,13 @@ package Network_Utill;
 
 
 import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.Proxy;
 import java.net.URL;
 
@@ -15,17 +18,21 @@ public class ExpanderNetwork extends AsyncTask<String, Void, String> {
     protected String doInBackground(String... params) {
 
         String expand_URL = "";
+
         try {
             URL url = new URL(params[0]);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection(Proxy.NO_PROXY);
             connection.setInstanceFollowRedirects(false);
             connection.connect();
-
             if (connection.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND)
                 expand_URL = "notFound"; // 없는 페이지일 시
-            else
-                expand_URL = connection.getHeaderField("Location");
 
+            else {
+                if(TextUtils.isEmpty(connection.getHeaderField("Location")))
+                    expand_URL = "notFound"; // 최근 접속 경로 찾지못함
+                else
+                    expand_URL = connection.getHeaderField("Location");
+            }
             connection.getInputStream().close(); // 연결 종료
         } catch (MalformedURLException e) {
             System.err.println("ExpanderNetwork MalformedURLException");
